5272f23f9a3a7d51099a612205ee5aaf6c832045
unixjazz <473291+unixjazz@users.noreply.github.com>
2024-07-08 05:46:17 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for
   # options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use
   # secure cookies.
